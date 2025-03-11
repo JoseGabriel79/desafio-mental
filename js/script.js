d023bebc1d71ab74010860c944697cf076983f3c
jamesdasaladadefruta <maycoldaveld@gmail.com>
2025-03-11 16:52:28 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
 var listaDeTarefas = [
     {
         tarefa: "Dormir",
-        prazo: new Date(2025, 2, 11, 11, 0, 0),
+        prazo: new Date(2025, 2, 11),
     },
     {
         tarefa: "Correr",
-        prazo: new Date(2025, 2, 11, 13, 30, 0),
+        prazo: new Date(2025, 2, 11),
     },
 ]
 
 
 function cadastrarTarefa(){
-    let novaTarefa = prompt("Qual o nome da Tarefa? ")
-    let novoAno = prompt("Qual ano?")
-    let novoMes = prompt("Qual mes?")
-    let novoDia = prompt("Qual dia?")
-    let novoHora = prompt("Qual hora?")
-    alert( "A tarefa: " + tarefa.tarefa + " com prazo até " + tarefa.prazo.toLocaleTimeString() + " Foi CADASTRADA!")
+    
+    let conteudoModalHTML = document.getElementById("modal-content")
+    let divModalHTML = document.createElement("div")
+    conteudoModalHTML.style.display = "flex"
+    conteudoModalHTML.style.flexDirection = "column-reverse"
+
+    divModalHTML.innerHTML = `
+        <h2>Cadastrar Tarefa</h2>
+        <input type = "text" placeholder = "Qual o nome da Tarefa?">
+        <input type="date" id="data" name="data" min="2025-01-01" max="3945-12-31">
+
+    `
+    conteudoModalHTML.appendChild(divModalHTML)
+    
+    // alert( "A tarefa: " + tarefa.tarefa + " com prazo até " + tarefa.prazo.toLocaleTimeString() + " Foi CADASTRADA!")
     
 }
 
@@ -27,8 +36,9 @@ function criarLista(){
         let liHTML = document.createElement("li")
 
         liHTML.innerHTML = `
-            ${tarefa.tarefa} ${tarefa.prazo.toLocaleTimeString()} <input type = "checkbox" class = "check">
-        `
+            ${tarefa.tarefa} - Prazo: ${tarefa.prazo.toLocaleDateString()} 
+            <input type="checkbox" class="check">
+        `;
     ulHTML.appendChild(liHTML)
     });
 
@@ -37,3 +47,41 @@ criarLista()
 
 
 
+// Obtendo os elementos
+var modalPrincipal = document.getElementById("modalPrincipal");
+var modalSecundario = document.getElementById("modalSecundario");
+var btnAbrirModalPrincipal = document.getElementById("abrirModalPrincipalBtn");
+var btnAbrirModalSecundario = document.getElementById("abrirModalSecundarioBtn");
+var btnFecharModalSecundario = document.getElementById("fecharSecundarioBtn");
+var btnFecharModalPrincipal = document.getElementsByClassName("fechar-modal");
+
+// Abrir o modal principal
+btnAbrirModalPrincipal.onclick = function() {
+    modalPrincipal.style.display = "block";
+    cadastrarTarefa()
+}
+
+// Fechar o modal principal
+btnFecharModalPrincipal[0].onclick = function() {
+    modalPrincipal.style.display = "none";
+}
+
+// Abrir o modal secundário dentro do modal principal
+btnAbrirModalSecundario.onclick = function() {
+    modalSecundario.style.display = "block";
+}
+
+// Fechar o modal secundário
+btnFecharModalSecundario.onclick = function() {
+    modalSecundario.style.display = "none";
+}
+
+// Fechar o modal principal ao clicar fora do conteúdo
+window.onclick = function(event) {
+    if (event.target == modalPrincipal) {
+        modalPrincipal.style.display = "none";
+    }
+    if (event.target == modalSecundario) {
+        modalSecundario.style.display = "none";
+    }
+}
