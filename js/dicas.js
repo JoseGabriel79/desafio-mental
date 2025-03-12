442bd2a383a129810d7ca242fa8a9a7604e94702
José Gabriel <josegabriel2001biel@gmail.com>
2025-03-12 13:49:25 -0300

    Create dicas.js

---
+++ js/dicas.js
@@ -0,0 +1,9 @@
+dicas = [
+    {
+        situacao: "Estresse",
+        dicas:["Meditação","Respiração diafragmática","Relaxamento muscular progressivo", "Treino autógeno de Schultz",
+        "Busque apoio","Mantenha contato físico positivo","Crie ambientes calmos","Use aromas relaxantes","Tenha um animal de estimação",
+            "Alimente- se corretamente"
+        ]
+    }
+]
