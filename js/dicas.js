78e3c1134ab3fa5e90ddd2d9433538941bd18263
José Gabriel <josegabriel2001biel@gmail.com>
2025-03-12 13:52:08 -0300

    Update dicas.js

--- js/dicas.js
+++ js/dicas.js
@@ -6,4 +6,11 @@ dicas = [
             "Alimente- se corretamente"
         ]
     }
+    {
+        situacao: "Estresse",
+        dicas:["Meditação","Respiração diafragmática","Relaxamento muscular progressivo", "Treino autógeno de Schultz",
+        "Busque apoio","Mantenha contato físico positivo","Crie ambientes calmos","Use aromas relaxantes","Tenha um animal de estimação",
+            "Alimente- se corretamente"
+        ]
+    }
 ]
