3ec1740bba6f6d4fb2fbb94774a3728ac57f5531
José Gabriel <josegabriel2001biel@gmail.com>
2025-03-12 14:45:51 -0300

    Update dicas.js

--- js/dicas.js
+++ js/dicas.js
@@ -1,39 +1,52 @@
 dicas = [
     {
         situacao: "Estresse",
-        dicas:["Meditação","Respiração diafragmática","Relaxamento muscular progressivo", "Treino autógeno de Schultz",
-        "Busque apoio","Mantenha contato físico positivo","Crie ambientes calmos","Use aromas relaxantes","Tenha um animal de estimação",
+        dicas: ["Meditação", "Respiração diafragmática", "Relaxamento muscular progressivo", "Treino autógeno de Schultz",
+            "Busque apoio", "Mantenha contato físico positivo", "Crie ambientes calmos", "Use aromas relaxantes", "Tenha um animal de estimação",
             "Alimente- se corretamente"
         ]
     },
     {
         situacao: "Irritação",
-        dicas:["Meditar","Praticar yoga","Fazer acupuntura", "Fazer massagem",
-        "Respirar profundamente","Contar até 10","Escrever sobre o problema"]
+        dicas: ["Meditar", "Praticar yoga", "Fazer acupuntura", "Fazer massagem",
+            "Respirar profundamente", "Contar até 10", "Escrever sobre o problema"]
     }
 ]
 
-function criarListaDeDicas(){
+function criarListaDeDicas() {
     let testeDicasHTML = document.getElementById("testeDicas")
-    let divOlHtml = document.createElement("ol")
-    let dicasSalvas = ""
+
     dicas.forEach(item => {
-        item.dicas.forEach(dica=>{
-            dicasSalvas.innerHTML = `
-                <li>${dica}</li>
-            `
+
+        let h2Situacao = document.createElement("h2")
+        h2Situacao.textContent = item.situacao
+        let divOlHtml = document.createElement("ol")
+
+        item.dicas.forEach(dica => {
+            let li = document.createElement("li")
+            li.textContent = dica
+            divOlHtml.appendChild(li)
         })
-        divOlHtml.innerHTML = `
-            <h2>${item.situacao}</h2>
-            ${dicasSalvas}
-        `
-        
+
+        testeDicasHTML.appendChild(h2Situacao)
+        testeDicasHTML.appendChild(divOlHtml)
     });
 
 
 }
 
+// function mostrarDicas() {
+//     opcaoDicasHTML.style.display = "none"
+//     criarListaDeDicas()
+// }
+let opcaoDicasHTML = document.getElementById("opcaoDicas")
 
 
+
+let mostrarDicasHTML = document.getElementById("mostrarDicas")
+mostrarDicasHTML.onclick = function () {
+    opcaoDicasHTML.style.display = "none";
+    criarListaDeDicas()
+}
 
 
