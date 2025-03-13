92a060a8c4c437a59b61a7265f9a1e49d9e41c70
jamesdasaladadefruta <maycoldaveld@gmail.com>
2025-03-13 15:03:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,335 @@
+// // var listaDeTarefas = [
+// //     {
+// //         tarefa: "Dormir",
+// //         prazo: new Date(2025, 2, 11),
+// //     },
+// //     {
+// //         tarefa: "Correr",
+// //         prazo: new Date(2025, 2, 11),
+// //     },
+// // ]
+
+
+// // function criarMenuCadastroTarefa(){
+// //     let conteudoModalHTML = document.getElementById("modal-content")
+// //     let divModalHTML = document.createElement("div")
+// //     conteudoModalHTML.style.display = "flex"
+// //     conteudoModalHTML.style.flexDirection = "column-reverse"
+
+// //     divModalHTML.innerHTML = `
+// //         <h1>Cadastrar Tarefa</h1>
+// //         <input id = "inptTarefa" type = "text" placeholder = "Qual o nome da Tarefa?">
+// //         <input id = "inptPrazo" type="date" id="data" name="data" min="2025-01-01" max="3945-12-31">
+// //         <button onclick = "finalizarCadastro()" id="abrirModalSecundarioBtn">Cadastrar</button>
+// //     `
+// //     conteudoModalHTML.innerHTML = " "
+// //   conteudoModalHTML.appendChild(divModalHTML)
+
+  
+// // }
+
+// // function criarLista(){
+// //     let ulHTML = document.getElementById("listaTarefas")
+// //     ulHTML.innerHTML = ""
+    
+// //     listaDeTarefas.forEach(tarefa => {
+// //         let liHTML = document.createElement("li")
+        
+// //         liHTML.innerHTML = `
+// //         <span>
+// //             <h4>${tarefa.tarefa} - Prazo: ${tarefa.prazo.toLocaleDateString()}</h4>
+// //             <input value="${index}" onclick = "excluirTarefa()" type="checkbox" class="check openModalBtn">
+// //         </span>
+        
+// //         `;
+// //         ulHTML.appendChild(liHTML)
+// //     });
+    
+// // }
+
+
+// // function finalizarCadastro() {
+// //     let inputTarefaHTML = document.getElementById("inptTarefa").value
+// //     let inputPrazoHTML = document.getElementById("inptPrazo").value
+// //     if(inputPrazoHTML == "" || inputPrazoHTML){
+
+// //     }
+// //     let prazoConvertido = new Date(inputPrazoHTML)
+// //    let novaTarefa = {
+// //     tarefa: inputTarefaHTML,
+// //     prazo: prazoConvertido
+// //    }
+
+// //    listaDeTarefas.push(novaTarefa)
+// //     modalSecundario.style.display = "block";
+    
+// //     // Fechar o modal secundário automaticamente após 3 segundos
+// //     setTimeout(function() {
+// //         modalSecundario.style.display = "none";
+// //         modalPrincipal.style.display = "none";
+// //     }, 5000); // 3000 milissegundos = 3 segundos
+
+// //     criarLista()
+// // }
+
+
+
+// // criarLista()
+
+
+
+// // // Obtendo os elementos
+// // var modalPrincipal = document.getElementById("modalPrincipal");
+// // var modalSecundario = document.getElementById("modalSecundario");
+// // var btnAbrirModalPrincipal = document.getElementById("abrirModalPrincipalBtn");
+// // var btnAbrirModalSecundario = document.getElementById("abrirModalSecundarioBtn");
+// // var btnFecharModalSecundario = document.getElementById("fecharSecundarioBtn");
+// // var btnFecharModalPrincipal = document.getElementsByClassName("fechar-modal");
+
+// // // Abrir o modal principal
+// // btnAbrirModalPrincipal.onclick = function() {
+// //     modalPrincipal.style.display = "block";
+// //     criarMenuCadastroTarefa()
+// // }
+
+// // // Fechar o modal principal
+// // btnFecharModalPrincipal[0].onclick = function() {
+// //     modalPrincipal.style.display = "none";
+// // }
+
+
+// // // Fechar o modal secundário
+// // btnFecharModalSecundario.onclick = function() {
+// //     modalSecundario.style.display = "none";
+// // }
+
+// // // Fechar o modal principal ao clicar fora do conteúdo
+// // window.onclick = function(event) {
+// //     if (event.target == modalPrincipal) {
+// //         modalPrincipal.style.display = "none";
+// //     }
+// //     if (event.target == modalSecundario) {
+// //         modalSecundario.style.display = "none";
+// //     }
+// // }
+
+
+// var listaDeTarefas = [
+//     {
+//         tarefa: "Dormir",
+//         prazo: new Date(2025, 2, 11),
+//     },
+//     {
+//         tarefa: "Correr",
+//         prazo: new Date(2025, 2, 11),
+//     },
+// ]
+
+
+
+// function criarMenuCadastroTarefa() {
+//     let conteudoModalHTML = document.getElementById("modal-content")
+//     let divModalHTML = document.createElement("div")
+//     conteudoModalHTML.style.display = "flex"
+//     conteudoModalHTML.style.flexDirection = "column-reverse"
+
+//     divModalHTML.innerHTML = `
+//         <h1>Cadastrar Tarefa</h1>
+//         <input id = "inptTarefa" type = "text" placeholder = "Qual o nome da Tarefa?">
+//         <input id = "inptPrazo" type="date" id="data" name="data" min="2025-01-01" max="3945-12-31">
+//         <button onclick = "finalizarCadastro()" id="abrirModalSecundarioBtn">Cadastrar</button>
+
+//     `
+//     conteudoModalHTML.innerHTML = " "
+//     conteudoModalHTML.appendChild(divModalHTML)
+   
+// }
+
+
+
+
+
+// function criarLista() {
+//     let ulHTML = document.getElementById("listaTarefas")
+//     ulHTML.innerHTML = ""
+    
+  
+//     listaDeTarefas.forEach((tarefa, index) => {
+//         let liHTML = document.createElement("li")
+        
+
+//         liHTML.innerHTML = `
+//         <span>
+//             <h4>${tarefa.tarefa} - Prazo: ${tarefa.prazo.toLocaleDateString()}</h4>
+//             <input value="${index}" onclick = "excluirTarefa()" type="checkbox" class="check openModalBtn">
+//         </span>
+//         `
+        
+//         ulHTML.appendChild(liHTML)
+//     });
+    
+
+
+//     const openModalBtn = document.getElementsByClassName("openModalBtn")
+
+//     for (i = 0; i < listaDeTarefas.length; i++) {
+//         openModalBtn[i].addEventListener("click", function () {
+//             modalContainer.style.display = "block";
+//         });
+
+//     }
+// }
+
+
+// function finalizarCadastro() {
+//     let inputTarefaHTML = document.getElementById("inptTarefa").value
+//     let inputPrazoHTML = document.getElementById("inptPrazo").value
+//     let prazoConvertido = new Date(inputPrazoHTML)
+//    let novaTarefa = {
+
+//     tarefa: inputTarefaHTML,
+//     prazo: prazoConvertido,
+//    }
+   
+   
+//     listaDeTarefas.push(novaTarefa)
+//     modalSecundario.style.display = "block";
+    
+
+//     // Fechar o modal secundário automaticamente após 3 segundos
+   
+//     setTimeout(function () {
+     
+//         modalPrincipal.style.display = "none";
+//     }, 5000); // 3000 milissegundos = 3 segundos
+
+
+
+//     criarLista()
+// };
+
+// criarLista()
+
+
+
+// function excluirTarefa() {
+//     let checkboxes = document.querySelectorAll(".check")
+//     checkboxes.forEach(checkbox => {
+//         checkbox.addEventListener('change', function () {
+//             if (checkbox.checked) {
+//                 // Remover o item da lista (o <li> que contém o checkbox)
+//                 const item = checkbox.parentElement;
+//                 listaDeTarefas.splice(checkbox.value, 1)
+//                 item.remove();
+//             }
+//         });
+//     });
+// }
+
+
+// // Obtendo os elementos
+// var modalPrincipal = document.getElementById("modalPrincipal");
+
+//  var btnFecharModalSecundario = document.getElementById("fecharSecundarioBtn");
+// var btnFecharModalPrincipal = document.getElementsByClassName("fechar-modal");
+
+// // Abrir o modal principal
+
+// btnAbrirModalPrincipal.onclick = function () {
+//     modalPrincipal.style.display = "block";
+//     criarMenuCadastroTarefa()
+// }
+
+// // Fechar o modal principal
+
+// btnFecharModalPrincipal[0].onclick = function () {
+//     modalPrincipal.style.display = "none";
+// }
+
+
+// // Fechar o modal secundário
+
+// btnFecharModalSecundario.onclick = function () {
+//     modalSecundario.style.display = "none";
+// }
+
+// // Fechar o modal principal ao clicar fora do conteúdo
+
+// window.onclick = function (event) {
+//     if (event.target == modalPrincipal) {
+//         modalPrincipal.style.display = "none";
+//     }
+
+// window.onclick = function(event) {
+//         modalSecundario.style.display = "none";
+//     }
+// }
+
+// //MODAL CONCLUIR TAREFAS
+// // Obter elementos do DOM
+// const modalContainer = document.getElementById("modalContainer");
+// const closeModalBtn = document.getElementById("closeModalBtn");
+// const closeModalBtnFooter = document.getElementById("closeModalBtnFooter");
+
+
+
+// // Fechar o modal ao clicar no botão de fechar (X)
+// closeModalBtn.addEventListener("click", function () {
+//     modalContainer.style.display = "none";
+// });
+
+// // Fechar o modal ao clicar no botão "Fechar"
+// closeModalBtnFooter.addEventListener("click", function () {
+//     modalContainer.style.display = "none";
+// });
+
+// // Fechar o modal ao clicar fora do conteúdo do modal
+// window.addEventListener("click", function (event) {
+//     if (event.target === modalContainer) {
+//         modalContainer.style.display = "none";
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var listaDeTarefas = [
     {
         tarefa: "Dormir",
@@ -10,7 +342,7 @@ var listaDeTarefas = [
 ]
 
 
-function criarMenuCadastroTarefa(){
+function criarMenuCadastroTarefa() {
     let conteudoModalHTML = document.getElementById("modal-content")
     let divModalHTML = document.createElement("div")
     conteudoModalHTML.style.display = "flex"
@@ -23,59 +355,79 @@ function criarMenuCadastroTarefa(){
         <button onclick = "finalizarCadastro()" id="abrirModalSecundarioBtn">Cadastrar</button>
     `
     conteudoModalHTML.innerHTML = " "
-  conteudoModalHTML.appendChild(divModalHTML)
+    conteudoModalHTML.appendChild(divModalHTML)
+    // alert( "A tarefa: " + tarefa.tarefa + " com prazo até " + tarefa.prazo.toLocaleTimeString() + " Foi CADASTRADA!")
 
-  
 }
 
-function criarLista(){
+// function cadastrarTarefa(){
+
+// }
+
+function criarLista() {
     let ulHTML = document.getElementById("listaTarefas")
     ulHTML.innerHTML = ""
-    
-    listaDeTarefas.forEach(tarefa => {
+
+    listaDeTarefas.forEach((tarefa, index) => {
         let liHTML = document.createElement("li")
-        
+
         liHTML.innerHTML = `
         <span>
             <h4>${tarefa.tarefa} - Prazo: ${tarefa.prazo.toLocaleDateString()}</h4>
             <input value="${index}" onclick = "excluirTarefa()" type="checkbox" class="check openModalBtn">
-            <input type="checkbox" class="check" id="check"   onclick ="removerCampo()">
         </span>
-        
-        `;
+        `
         ulHTML.appendChild(liHTML)
     });
-    
-}
 
+    const openModalBtn = document.getElementsByClassName("openModalBtn")
+
+    for (i = 0; i < listaDeTarefas.length; i++) {
+        openModalBtn[i].addEventListener("click", function () {
+            modalContainer.style.display = "block";
+        });
+
+    }
+}
 
 function finalizarCadastro() {
     let inputTarefaHTML = document.getElementById("inptTarefa").value
     let inputPrazoHTML = document.getElementById("inptPrazo").value
-    if(inputPrazoHTML == "" || inputPrazoHTML){
-
-    }
     let prazoConvertido = new Date(inputPrazoHTML)
-   let novaTarefa = {
-    tarefa: inputTarefaHTML,
-    prazo: prazoConvertido
-   }
+    let novaTarefa = {
+        tarefa: inputTarefaHTML,
+        prazo: prazoConvertido
+    }
 
-   listaDeTarefas.push(novaTarefa)
+    listaDeTarefas.push(novaTarefa)
     modalSecundario.style.display = "block";
-    
+
     // Fechar o modal secundário automaticamente após 3 segundos
-    setTimeout(function() {
+    setTimeout(function () {
         modalSecundario.style.display = "none";
         modalPrincipal.style.display = "none";
     }, 5000); // 3000 milissegundos = 3 segundos
 
+  
+
     criarLista()
 }
-
 criarLista()
 
 
+function excluirTarefa() {
+    let checkboxes = document.querySelectorAll(".check")
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', function () {
+            if (checkbox.checked) {
+                // Remover o item da lista (o <li> que contém o checkbox)
+                const item = checkbox.parentElement;
+                listaDeTarefas.splice(checkbox.value, 1)
+                item.remove();
+            }
+        });
+    });
+}
 
 // Obtendo os elementos
 var modalPrincipal = document.getElementById("modalPrincipal");
@@ -86,24 +438,24 @@ var btnFecharModalSecundario = document.getElementById("fecharSecundarioBtn");
 var btnFecharModalPrincipal = document.getElementsByClassName("fechar-modal");
 
 // Abrir o modal principal
-btnAbrirModalPrincipal.onclick = function() {
+btnAbrirModalPrincipal.onclick = function () {
     modalPrincipal.style.display = "block";
     criarMenuCadastroTarefa()
 }
 
 // Fechar o modal principal
-btnFecharModalPrincipal[0].onclick = function() {
+btnFecharModalPrincipal[0].onclick = function () {
     modalPrincipal.style.display = "none";
 }
 
 
 // Fechar o modal secundário
-btnFecharModalSecundario.onclick = function() {
+btnFecharModalSecundario.onclick = function () {
     modalSecundario.style.display = "none";
 }
 
 // Fechar o modal principal ao clicar fora do conteúdo
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modalPrincipal) {
         modalPrincipal.style.display = "none";
     }
@@ -112,3 +464,27 @@ window.onclick = function(event) {
     }
 }
 
+//MODAL CONCLUIR TAREFAS
+// Obter elementos do DOM
+const modalContainer = document.getElementById("modalContainer");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const closeModalBtnFooter = document.getElementById("closeModalBtnFooter");
+
+
+
+// Fechar o modal ao clicar no botão de fechar (X)
+closeModalBtn.addEventListener("click", function () {
+    modalContainer.style.display = "none";
+});
+
+// Fechar o modal ao clicar no botão "Fechar"
+closeModalBtnFooter.addEventListener("click", function () {
+    modalContainer.style.display = "none";
+});
+
+// Fechar o modal ao clicar fora do conteúdo do modal
+window.addEventListener("click", function (event) {
+    if (event.target === modalContainer) {
+        modalContainer.style.display = "none";
+    }
+});
